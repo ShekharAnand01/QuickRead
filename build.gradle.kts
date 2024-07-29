1360e943a9ea8f9fadbb112910379beec11c2074
Shekhar Anand <shekharanand134@gmail.com>
2024-07-29 19:15:03 +0530

    Initial setup of the project

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+}
