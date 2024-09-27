c7651365bbe6e2c312f72f3824f872c9a3f3bb0d
Shekhar Anand <shekharanand134@gmail.com>
2024-09-27 17:57:13 +0530

    App adjusted for both dark and light theme

--- app/src/main/java/com/example/newsapp/screens/SearchFragment.kt
+++ app/src/main/java/com/example/newsapp/screens/SearchFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.AbsListView
 import android.widget.Button
 import android.widget.TextView
@@ -51,6 +52,8 @@ class SearchFragment : Fragment() {
         viewModel = (activity as NewsActivity).viewModel
         setUpAdapter()
         binding.searchEdit.requestFocus()
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.searchEdit, InputMethodManager.SHOW_IMPLICIT)
 
         itemSearchError = view.findViewById(R.id.itemSearchError)
         val inflater =
